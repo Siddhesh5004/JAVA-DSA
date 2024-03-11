2b3932d6a43a9dc0e39cbbbf431b185863e3378d
Siddhesh Bhelke <139620000+Siddhesh5004@users.noreply.github.com>
2024-03-11 20:38:00 +0530

    updated
    Added difference between while loop and for loop

--- 3. loops/Answer2.java
+++ 3. loops/Answer2.java
@@ -1,17 +1,17 @@
 
-    import java.util.Scanner;
-public class Answer2  {
-public static void main(String[] args) {
-Scanner scn = new Scanner(System.in);
-int num; // To hold number
-int fact = 1; // To hold factorial
-System.out.print("Enter any positive integer: ");
-num = scn.nextInt();
-for(int i=1; i<=num; i++) {
-fact *= i;
-}
-System.out.println("Factorial: "+ fact);
- scn.close();
-}
-}
+import java.util.Scanner;
 
+public class Answer2 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int num; // To hold number
+        int fact = 1; // To hold factorial
+        System.out.print("Enter any positive integer: ");
+        num = scn.nextInt();
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+        System.out.println("Factorial: " + fact);
+        scn.close();
+    }
+}
---
+++ 3. loops/while vs for loop.md
@@ -0,0 +1,20 @@
+public class whileloop{
+    public stativ void main(Strings args[]){
+
+        int i=1;
+        while(i<=5>){
+            System.out.println("*****");
+            i++;
+        }
+    }
+}
+
+
+public class forloop{
+    public static void main (Strings args[]){
+
+        for(int i=1; i<=5; i++) {
+            System.out.println("*****");
+        }
+    }
+}
