c7492a05bb8b2901f537bc9c18322f8bf06b81c1
Siddhesh Bhelke <139620000+Siddhesh5004@users.noreply.github.com>
2024-03-12 23:43:22 +0530

    updated loops

--- 3. loops/Answer1.java
+++ 3. loops/Answer1.java
@@ -1,28 +1,29 @@
- import java.util.Scanner;
+
+
+import java.util.Scanner;
+
 public class Answer1 {
-public static void main(String[] args) {
-Scanner scn = new Scanner(System.in);
-int number;
-int choice;
-int evenSum = 0;
-int oddSum = 0;
-do {
-System.out.print("Enter the number ");
-number = scn.nextInt();
-if( number % 2 == 0) {
-evenSum += number;
-} else {
-oddSum += number;
-}
-System.out.print("Do you want to continue? Press 1 for yes or 0 for no");
-choice = scn.nextInt();
-} while(choice==1);
-System.out.println("Sum of even numbers: " + evenSum);
-System.out.println("Sum of odd numbers: " + oddSum);
-   
-scn.close();
-}
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int number;
+        int choice;
+        int evenSum = 0;
+        int oddSum = 0;
+        do {
+            System.out.print("Enter the number ");
+            number = scn.nextInt();
+            if (number % 2 == 0) {
+                evenSum += number;
+            } else {
+                oddSum += number;
+            }
+            System.out.print("Do you want to continue? Press 1 for yes or 0 for no");
+            choice = scn.nextInt();
+        } while (choice == 1);
+        System.out.println("Sum of even numbers: " + evenSum);
+        System.out.println("Sum of odd numbers: " + oddSum);
 
+        scn.close();
+    }
 
 }
-
