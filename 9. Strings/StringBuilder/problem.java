2809db17e3af6c4e006fb71e226bf41285ecbb57
Siddhesh Bhelke <139620000+Siddhesh5004@users.noreply.github.com>
2024-04-17 11:59:54 +0530

    string builder

---
+++ 9. Strings/StringBuilder/problem.java
@@ -0,0 +1,22 @@
+
+// for a gievn string convert each first letter of each word to uppercase
+
+import java.util.Scanner;
+
+public class problem {
+
+    public static String toUppercase(String str){
+        StringBuilder sb= new StringBuilder("")
+
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+
+        for(int i=1;i<str.length();i++){
+            if(str.charAt(i))
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+         String = sc.nextLine();
+    }
+}
