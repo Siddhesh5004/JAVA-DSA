de235e0346974e7dd0989381ccffd60709925fa0
Siddhesh Bhelke <139620000+Siddhesh5004@users.noreply.github.com>
2024-06-07 17:25:14 +0530

    new updated

---
+++ 10. Bitmanipulation/Problem1.java
@@ -0,0 +1,20 @@
+// check if the number is even or odd 
+
+public class Problem1 {
+    public static void oddeven(int n ){
+        int bitmask=1;
+        if((n&bitmask)==0){
+            System.out.println("even number");
+            
+        }else{
+            System.out.println("odd number");
+        }
+    }
+    public static void main(String[] args) {
+        oddeven(5);
+        oddeven(10);
+        oddeven(13);
+    }
+}
+    
+
---
+++ 10. Bitmanipulation/bitoperator.java
@@ -0,0 +1,11 @@
+
+public class bitoperator {
+    public static void main(String[] args) {
+    System.out.println((5 & 6));    // AND (&) operator is used. ans=6
+    System.out.println(5|6); // OR (|) operator is used . ans=7
+    System.out.println(5^6);// XOR (^) operator is used . ans=3
+    System.out.println(~5); // one's complement is used . ans=-6
+    System.out.println(5<<2); // binary left shift is used . ans=20 
+    System.out.println(6>>1); // binary right shift is used . ans=3
+}
+ }
