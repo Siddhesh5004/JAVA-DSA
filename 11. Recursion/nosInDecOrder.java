126c74f27884162d232f34fcb8e8dd423db194b8
Siddhesh Bhelke <139620000+Siddhesh5004@users.noreply.github.com>
2024-06-22 22:24:17 +0530

    Recursion updated

--- 11. Recursion/nosInDecOrder.java
+++ 11. Recursion/nosInDecOrder.java
@@ -1,16 +1,16 @@
-public class test {
-
+public class nosInDecOrder {
     public static void printDec(int n) {
-        if (n == 1) { // Base case
-             
-             return;
+        if (n == 0) { // Base case
+            return;
         }
         System.out.print(n + " "); // Print the current number
         printDec(n - 1); // Recursive call with n-1
     }
 
     public static void main(String[] args) {
-        int n = 100;
+        int n = 10;
         printDec(n); // Call the method with n
     }
 }
+
+
