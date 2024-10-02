55d931ae04417f54fee65e5b2a4414e6cb7560a4
Siddhesh Bhelke <139620000+Siddhesh5004@users.noreply.github.com>
2024-10-02 21:26:38 +0530

    updated

---
+++ 14. Time and Space Complexity/loops_Complexity.java
@@ -0,0 +1,16 @@
+// simple one loop
+for (int i=0;i<n;i++){
+    // loop runs n times so TC is O(n)
+}
+
+
+// Nested Loop
+
+for(int i=0;i<n;i++){
+    for(int j=i+1;j<n;j++){
+        //some constant work (conditions)
+        //nested loop TC is worst case :  
+    }
+}
+
+
